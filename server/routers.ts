4b69359b1fcc5912f400b3d0fb807324c294ae28
Jaokimben <Jaokimben@users.noreply.github.com>
2026-01-19 20:35:37 +0000

    fix: FINAL SOLUTION - rename server/_core/index.ts to dev-server.ts to prevent Vercel auto-detection

--- server/routers.ts
+++ server/routers.ts
@@ -7,7 +7,7 @@ import * as db from "./db";
 import { initialActions } from "../client/src/lib/data";
 
 export const appRouter = router({
-    // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with '/api/' so that the gateway can route correctly
+    // if you need to use socket.io, read and register route in server/_core/dev-server.ts, all api should start with '/api/' so that the gateway can route correctly
   system: systemRouter,
   auth: router({
     me: publicProcedure.query(opts => opts.ctx.user),
